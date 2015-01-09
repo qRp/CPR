62d46923c773f7daa49e15471425be847d86eed8
Marjorie Mersch <marmersch@emi.u-bordeaux1.fr>
2015-01-09 14:51:58 +0100

    Main

--- Main.java
+++ Main.java
@@ -1,15 +1,4 @@
-import java.awt.Color;
-
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
