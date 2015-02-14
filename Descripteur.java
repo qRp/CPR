cbac24bbfc99f543143e02d797113b94e448b66c
Quentin Riche-Piotaix <querichepiotai@emi.u-bordeaux1.fr>
2015-02-14 10:59:23 +0100

    descripteur

--- Descripteur.java
+++ Descripteur.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Descripteur{
     private String name;
@@ -41,6 +42,33 @@ public class Descripteur{
     public float getvalue(){
         return value;
     }
+    public float getminV(){
+	return minV;
+    }
+    public float getmaxV(){
+	return maxV;
+    }
+    public float getpasV(){
+	return pasV;
+    }
+    public void setname(String nname){
+	    name=nname;
+    }
+    public void settype(String ttype){
+	    type=ttype;
+    }
+    public void setvalue(float vvalue){
+        value=vvalue;
+    }
+    public void setminV(float mminV){
+	minV=mminV;
+    }
+    public void setmaxV(float mmaxV){
+	maxV=mmaxV;
+    }
+    public void setpasV(float ppasV){
+	pasV=ppasV;
+    }
     public ArrayList<Float> getvalues(){
         lValue.add(value);
         lValue.add(minV);
@@ -48,4 +76,43 @@ public class Descripteur{
         lValue.add(pasV);
 	    return lValue;
     }
+    
+    public void ecrire(){
+	//on va chercher le chemin et le nom du fichier et on me tout ca dans un String
+	String adressedufichier = System.getProperty("user.dir") + "/Exp.txt";
+	try{
+		/**
+		 * BufferedWriter a besoin d un FileWriter, 
+		 * les 2 vont ensemble, on donne comme argument le nom du fichier
+		 * true signifie qu on ajoute dans le fichier (append), on ne marque pas par dessus 
+		 
+		 */
+		FileWriter fw = new FileWriter(adressedufichier, true);
+		// le BufferedWriter output auquel on donne comme argument le FileWriter fw cree juste au dessus
+		BufferedWriter output = new BufferedWriter(fw);
+		
+		//on marque dans le fichier ou plutot dans le BufferedWriter qui sert comme un tampon(stream)
+		output.write("name:"+getname()+"\n");
+		output.write("type:"+gettype()+"\n");
+		output.write("value:"+(String)getvalue()+"\n");
+		output.write("minV:"+(String)getminV()+"\n");
+		output.write("maxV:"+(String)getmaxV()+"\n");
+		output.write("pasV:"+(String)getpasV()+"\n");		
+
+		//on peut utiliser plusieurs fois methode write
+		
+		output.flush();
+		//ensuite flush envoie dans le fichier, ne pas oublier cette methode pour le BufferedWriter
+		
+		output.close();
+		//et on le ferme
+		System.out.println("fichier créé");
+	}
+	catch(IOException ioe){
+	    System.out.print("FATAL ERROR SYSTEM - Descripteur.java ");
+	    ioe.printStackTrace();
+	}
+	
+    }
 }
+
